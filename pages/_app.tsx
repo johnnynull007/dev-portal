4dd2d8da204e23516f542e5a0c8d87a1a9c37f04
Johnny <johnny@Johnnys-MacBook-Pro.local>
2023-03-10 13:51:07 +0100

    added back4app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,15 @@ import "./styles.css"
 
 import type { AppProps } from "next/app"
 import type { Session } from "next-auth"
+// Import Parse minified version
+import Parse from "parse"
+
+// Your Parse initialization configuration goes here
+const PARSE_APPLICATION_ID = 'g4CUc7NmgEMdYHzV3ZVRb8hjwxI4fKXmyyg5PMPr';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'bU0tRiCpluJfrAUd7xssaQrGyLZtXGPg9tIDOnYU';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
--- pages/me.tsx
+++ pages/me.tsx
@@ -1,13 +1,24 @@
 import { useSession } from "next-auth/react"
 import Layout from "../components/layout"
+import Parse from "parse"
 
 export default function MePage() {
   const { data } = useSession()
+  async function fetchPerson() {
+    console.log('Running')
+    // create your Parse Query using the Person Class you've created
+    const query = new Parse.Query('kudos');
+    // run the query
+    const kudo = await query.first();
+    console.log('person name: ', kudo.get('from'));
+    // access the Parse Object attributes
+  }
+
+  fetchPerson();
 
   return (
     <Layout>
       <pre>{JSON.stringify(data, null, 2)}</pre>
-      {console.log(data)}
     </Layout>
   )
 }
